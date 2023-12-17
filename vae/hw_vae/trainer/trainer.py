f16e26cc9678275d472539cb3f877de2cdf338d3
Pe4enkazAMI <yavmaksimov@edu.hse.ru>
2023-12-17 14:27:08 +0300

    lolka

--- vae/hw_vae/trainer/trainer.py
+++ vae/hw_vae/trainer/trainer.py
@@ -149,7 +149,7 @@ class Trainer(BaseTrainer):
 
         with torch.no_grad():
             fake = self.model.generate(self.fixed_noise[0, ...].unsqueeze(0)).detach().cpu()[0, ...]
-            fake = T.ToPILImage()(torch.clip(fake, min=-1, max=1))
+            fake = ((torch.clip(fake, min=-1, max=1)) + 1) * 255 / 2
             self.writer.add_image("example_images", fake)
         return log
     
